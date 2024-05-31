a21c6834269c1267b3eeb72015a7827fcfd257c8
mfadhillll <95394393+ucupofcoffee@users.noreply.github.com>
2024-05-31 11:25:46 +0700

    Delete

--- website(template)/resources/views/layouts/navbars/navbar.blade.php
+++
@@ -1,7 +0,0 @@
-@auth()
-    @include('layouts.navbars.navs.auth', ['title' => $title])
-@endauth
-    
-@guest()
-    @include('layouts.navbars.navs.guest')
-@endguest
--- website/resources/views/layouts/navbars/navbar.blade.php
+++
@@ -1,3 +0,0 @@
-@auth
-    @include('layouts.navbars.navs.auth', ['title' => $title])
-@endauth
