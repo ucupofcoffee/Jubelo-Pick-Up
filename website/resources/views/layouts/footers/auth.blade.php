d390e3c9f2a3873f9951a40215765700fe4d08b8
mfadhillll <95394393+ucupofcoffee@users.noreply.github.com>
2024-05-31 11:36:11 +0700

    Delete

--- website/resources/views/layouts/footers/auth.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-    <div>
-        <div class="copyright" style="color: #8898AA;">
-            &copy; {{ now()->year }} {{ __('Made with') }} <i class="fas fa-heart text-red"></i> {{ __('by') }}
-            <a>{{ __('KoTA 104') }}</a>
-        </div>
-    </div>
-</footer>
